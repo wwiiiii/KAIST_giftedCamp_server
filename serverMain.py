ad8b62c419dd320f85b4bd01e6b9c4427f95cb16
wwiiiii <wwiiiii@kaist.ac.kr>
2016-08-01 17:21:06 +0900

    -

--- serverMain.py
+++ serverMain.py
@@ -2,10 +2,9 @@
 from flask import Flask, render_template, request, session, send_file, send_from_directory, redirect, url_for
 from werkzeug.wrappers import BaseRequest, BaseResponse
 from urlparse import urlparse
-from PIL import Image
 import sys, time, io, os
 import urllib
-import i2sMain, gradeScore
+import gradeScore
 
 reload(sys)
 
@@ -35,35 +34,10 @@ def home():
 def about():
   return render_template('about.html')
 
-@app.route('/notescan')
-def notescan():
-	print 'asdf'
-	return render_template('notescan.html')
 
 @app.route('/grade')
 def grade():
 	return render_template('grade.html')
-
-@app.route('/notecalc',methods = ['POST'])
-def notecalc():
-	print 'note'
-	print request
-	if request.method != 'POST':
-		return 'Access Denied'
-	print request.form
-	imgpath = request.form['imgpath']; lang = request.form['lang'];#.encode('utf-8')
-	autoRotate = int(request.form['rotate']);
-	if lang != 'eng' and lang != 'kor':
-		return '현재 지원되는 문자는 [알파벳 : eng / 한글 : kor]입니다.'
-	print uri_validator(imgpath)
-	if uri_validator(imgpath) == False:
-		return '유효하지 않은 URL입니다.'
-	imgname = str(time.time())
-	res, img = i2sMain.i2sWrapper(imgpath, lang, autoRotate); img.save(imgname+'.jpeg');
-	time.sleep(2)
-	myimgpath = 'http://52.78.66.95:12345/showimg/';
-	print res; return render_template('noteshow.html', resultstr=res.split('\n'), imgpath = myimgpath+imgname+'.jpeg');
-
 
 @app.route('/gradecalc',methods = ['POST'])
 def gradecalc():
@@ -74,38 +48,6 @@ def gradecalc():
 	code= request.form['code'];
 	foldername = str(time.time())
 	return str(gradeScore.gradeScore(code))
-
-
-
-@app.route('/showimg/<filename>')
-def showimg(filename):
-	return send_file(filename,mimetype='image/jpeg')
-
-@app.route('/uploadedimg/<filename>')
-def showuploadedimg(filename):
-	return send_file(UPLOAD_FOLDER + '\\\\' + filename,mimetype='image/jpeg')
-
-@app.route("/upload", methods=['GET', 'POST'])
-def uploadf():
-	print request
-	if request.method == 'POST':
-		file = request.files['file']
-		if file:
-			filename = file.filename
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return render_template('notescan.html', mypath = 'myfile:'+filename);
-	res = """
-	<!doctype html>
-	<title>Upload new File</title>
-	<h1>Upload new File</h1>
-	<form action="" method=post enctype=multipart/form-data>
-	  <p><input type=file name=file>
-		<input type=submit value=Upload>
-	</form>
-	<p>%s</p>
-	""" % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
-	print res
-	return res
 
 if __name__ == "__main__":
 	if len(sys.argv) == 1:
